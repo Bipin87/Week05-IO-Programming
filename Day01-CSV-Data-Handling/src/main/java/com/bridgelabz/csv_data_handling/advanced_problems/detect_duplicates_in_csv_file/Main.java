8b408522f15718b7113cba6338f219069508f0c6
Bipin87 <sahubipin484@gmail.com>
2025-02-19 12:02:31 +0530

    [Bipin Kumar Sahu] Add: detect duplicates in csv file code file added 🐛

---
+++ Day01-CSV-Data-Handling/src/main/java/com/bridgelabz/csv_data_handling/advanced_problems/detect_duplicates_in_csv_file/Main.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.csv_data_handling.advanced_problems.detect_duplicates_in_csv_file;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //using try and catch to handle exception if occurs
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+
+            //get user input for file name
+            System.out.print("Enter source file name: ");
+            String sourceFileName = br.readLine();
+
+            DetectDuplicatesInCSVFile.detectDuplicates(sourceFileName);
+
+        }catch (IOException e){
+            System.out.println("Exception caught "+ e.getMessage());
+        }
+    }
+
+}
